000fdf7d8718a419df2034ba066ca6e88e524d6f
Shaik Mohd Huzaifa <shaikmohdhuz@gmail.com>
2023-09-28 23:03:38 +0530

    firs

---
+++ src/APIs/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.SUPABASEURL;
+const supabaseKey = process.env.SUPABASEKEY;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
