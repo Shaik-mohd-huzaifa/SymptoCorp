19a9c3b5e16558c47336fc304e2a9824f61790e7
Shaik Mohd Huzaifa <shaikmohdhuz@gmail.com>
2023-09-29 16:18:07 +0530

    AddPublic Key

--- src/APIs/supabase.js
+++ src/APIs/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://gubymwuuroyagpdpgdif.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd1Ynltd3V1cm95YWdwZHBnZGlmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5NDI2MTkyNSwiZXhwIjoyMDA5ODM3OTI1fQ.qCGoYbeB8kXYtGdpqi26eVPYDyQUEvPaco6GGYD4Lq0";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd1Ynltd3V1cm95YWdwZHBnZGlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQyNjE5MjUsImV4cCI6MjAwOTgzNzkyNX0.pKlgHkkI0l3mX2HCi988LyC5mos9k1QV1yLnU-8ssUk";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
